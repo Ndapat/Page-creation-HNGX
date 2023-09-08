b3f88c5caec6dcf69dff8a78601f7cd911cedb2b
PATIENCE JOSEPH <125637124+Ndapat@users.noreply.github.com>
2023-09-08 08:53:16 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
-//declare the array
-const   daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-//function to update the current day of the week
-function updateDayOfWeek() {
-    const currentDate = new Date();
-    const dayOfWeek = days[currentDate.getUTCDay()];
-
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = dayOfWeek;
-}
-//function to update the current utc time
-function updateUTCTime() {
-   const currentDate = new Date();
-   const utcTime = currentDate
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = utcTime
-}
-//call the update function every second
-setInterval(updateDayOfWeek, 1000);
-setInterval(updateUTCTime, 1000);
+//function of time in milliseconds 
+const currentUTCTimeMillis = Date.now();
+        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        document.getElementById("currentUTCTime").textContent = currentUTCTimeMillis;
+//function of date
+        const currentDate = new Date();
+        const dayOfWeek = currentDate.getDay();
+        const currentDayOfTheWeek = daysOfWeek[dayOfWeek]
+        document.getElementById("currentDayOfTheWeek").textContent = currentDayOfTheWeek;
+        console.log(dayOfWeek)
