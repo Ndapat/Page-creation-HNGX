92e8dce57b5adc6475b654e2a9497d8b96e8ebb9
PATIENCE JOSEPH <125637124+Ndapat@users.noreply.github.com>
2023-09-08 19:22:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-// Function to update the current UTC time including milliseconds
-function updateUTCTime() {
+// Function to update the current date, time, and day of the week
+function updateDateTime() {
     const currentDate = new Date();
-    const utcTime = currentDate
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = utcTime;
+    const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][currentDate.getUTCDay()];
+    const utcTime = currentDate.toISOString().substr(11, 8); // Extract HH:MM:SS from UTC string
+    const dateElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const timeElement = document.querySelector('[data-testid="currentUTCTime"]');
+
+    dateElement.textContent = `Current Day: ${dayOfWeek}`;
+    timeElement.textContent = `Current Time (UTC): ${utcTime}`;
 }
 
-// Call the update functions initially and then every millisecond to keep the data rea
-setInterval(updateDayOfTheWeek, 1000);
-setInterval(updateUTCTime, 1); // Update every millisecond
+// Call the update function initially and then every second to keep the data real-time
+updateDateTime();
+setInterval(updateDateTime, 1000);
+        
